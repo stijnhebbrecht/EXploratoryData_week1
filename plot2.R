4a69e8f20a4d4720060f88cf2534e1f9fc3b086f
stijnhebbrecht <stijnhebbrecht@hotmail.com>
2020-03-29 13:48:36 +0200

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(grid)
+
+##load data
+if(file.exists("household_power_consumption.txt")) {
+  data0 <- read.table("household_power_consumption.txt",
+                      sep = ";", header = TRUE, stringsAsFactors = FALSE)
+} else if(file.exists("exdata-data-household_power_consumption.zip")) {
+  data0 <- read.table(unz("exdata-data-household_power_consumption.zip",
+                          "household_power_consumption.txt"),
+                      sep = ";", header = TRUE, stringsAsFactors = FALSE)
+} else {
+  temp <- tempfile()
+  download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip",temp)
+  data <- read.table(unz(temp, "household_power_consumption.txt"),
+                     sep = ";", header = TRUE, stringsAsFactors = FALSE)
+  unlink(temp)
+}
+
+##convert file
+data0 <- tbl_df(data0)
+dataFil <- filter(data, Date == "2/2/2007" | Date == "1/2/2007")
+dataFil <- mutate(dataFil, Date_Time = paste(Date, Time, sep = " "))
+dataFil$Date_Time <- strptime(dataFil$Date_Time, "%d/%m/%Y %H:%M:%S")
+dataFil[, 3:9] <- lapply(dataFil[, 3:9], as.numeric)
+dataFil <- dataFil[, c(10, 3:9)]
+
+##plot
+
+plot(dataFil$Date_Time, dataFil$Global_active_power, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
